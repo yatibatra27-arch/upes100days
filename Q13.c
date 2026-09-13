c704c82b02db0030efb309c286f5fc623494ef1f
yatibatra27-arch <yatibatra27@gmail.com>
2026-09-13 17:22:20 +0530

    Create Q13.c

---
+++ Q13.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int year;
+
+    scanf("%d", &year);
+
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+        printf("Leap Year\n");
+    else
+        printf("Not a Leap Year\n");
+
+    return 0;
+}
