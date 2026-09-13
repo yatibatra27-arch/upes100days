c93accd6f498d1645f508999bcd7e77d302a5956
yatibatra27-arch <yatibatra27@gmail.com>
2026-09-13 19:57:18 +0530

    Create Q38.c

---
+++ Q38.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, digit, sum = 0;
+
+    scanf("%d", &n);
+
+    while(n != 0)
+    {
+        digit = n % 10;
+        sum = sum + digit;
+        n = n / 10;
+    }
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
