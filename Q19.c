145624902e873f2ce3e44f632afe8bc6028a197a
yatibatra27-arch <yatibatra27@gmail.com>
2026-09-13 19:30:54 +0530

    Create Q19.c

---
+++ Q19.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a == b && b == c)
+        printf("Equilateral Triangle\n");
+    else if (a == b || b == c || a == c)
+        printf("Isosceles Triangle\n");
+    else
+        printf("Scalene Triangle\n");
+
+    return 0;
+}
